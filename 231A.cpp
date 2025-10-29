885e6bc01946b269a3ef6f8edc80b5c5d0649f26
Karol Nowocień <newshadowcharles@gmail.com>
2025-10-29 08:17:32 +0100

    231A

---
+++ 231A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int answer = 0;
+  for (int i = 0; i < n; i++)
+  {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a + b + c) >= 2)
+    {
+      answer++;
+    }
+  }
+  cout << answer;
+  return 0;
+}
